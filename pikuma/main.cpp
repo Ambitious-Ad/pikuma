295f6e7fec074d2b306c6e6cccb47b2cd3c326df
wolf.exe <aaron.kavanaugh@outlook.com>
2025-08-27 12:52:03 -0700

    Created .h .cpp files to better organize code for furthur implmitation

--- pikuma/main.cpp
+++ pikuma/main.cpp
@@ -1,34 +1,11 @@
-#include <iostream>
-#include <SDL3/SDL.h>
-
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+#include "Renderer.h"
 
 int main(int argc, char* args[])
 {
-	SDL_Window* window = nullptr;
-	SDL_Surface* screenSurface = nullptr;
+	Renderer renderer;
 
-	if (!SDL_Init(SDL_INIT_VIDEO))
-	{
-		SDL_Log("Window could not be created, SDL_Error: %s\n", SDL_GetError());
-	}
-	else
-	{
-		window = SDL_CreateWindow("Hi, mom!", SCREEN_WIDTH, SCREEN_HEIGHT, 0);
-		if (window == nullptr)
-		{
-			SDL_Log("Window could not be created, SDL_Error: %s\n", SDL_GetError());
-		}
-		screenSurface = SDL_GetWindowSurface(window);
-		SDL_FillSurfaceRect(screenSurface, nullptr, SDL_MapSurfaceRGB(screenSurface, 0xFF, 0xFF, 0xFF));
-
-		SDL_UpdateWindowSurface(window);
-		SDL_Delay(3000);
-	}
-
-	SDL_DestroyWindow(window);
-	SDL_Quit();
+	renderer.run();
+	renderer.close();
 
 	return 0;
 }
